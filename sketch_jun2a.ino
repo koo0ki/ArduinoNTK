4b28a531023b2084316dd5e753336b9ea4c276aa
koo0ki <kakoytoaleksandr@gmail.com>
2025-06-02 13:25:26 +0700

    slots

--- sketch_jun2a.ino
+++ sketch_jun2a.ino
@@ -1,11 +1,17 @@
-#define red 2
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include "CustomCharacters.h" // Импортируем файл с символами
+#include "SlotMachine.h"      // Импортируем файл с логикой слотов
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Устанавливаем дисплей
 
 void setup() {
-  pinMode(red, OUTPUT);
-  digitalWrite(red, true);
-
+    lcd.init();
+    lcd.backlight(); // Включаем подсветку дисплея
+    createCustomCharacters(lcd); // Создаем пользовательские символы
+    lcd.clear();
 }
 
 void loop() {
-  
+    runSlotMachine(lcd); // Запускаем анимацию слотов
 }
