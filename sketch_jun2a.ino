9b8fffc612ad1ea5fd0d7ea8187bf8e459bc9e03
koo0ki <kakoytoaleksandr@gmail.com>
2025-06-03 11:01:13 +0700

    mp3 player

--- sketch_jun2a.ino
+++ sketch_jun2a.ino
@@ -1,53 +1,67 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
 
-// Инициализация дисплеев
-LiquidCrystal_I2C lcd1(0x26, 16, 2);  // Первый дисплей (глаза)
-LiquidCrystal_I2C lcd2(0x27, 16, 2);  // Второй дисплей (улыбка)
+LiquidCrystal_I2C lcd1(0x26, 16, 2);
+LiquidCrystal_I2C lcd2(0x27, 16, 2);  
+SoftwareSerial mySoftwareSerial(10, 11); 
+DFRobotDFPlayerMini myDFPlayer;
 
 void setup() {
   lcd1.init();
-  lcd1.backlight();  // Включаем подсветку первого дисплея
+  lcd1.backlight();
   lcd2.init();
-  lcd2.backlight();  // Включаем подсветку второго дисплея
+  lcd2.backlight();
+  
+  mySoftwareSerial.begin(9600);
+  Serial.begin(115200);
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {
+    Serial.println(F("DFPlayer Mini не удалось инициализировать"));
+  }
+  
+  playSound(2)
 }
 
 void loop() {
-  // Выводим глаза на первом дисплее
+  // Анимация глаз
+  displayEyes();
+  
+  // Анимация улыбки
+  displaySmile();
+}
+
+void displayEyes() {
   lcd1.clear();
   lcd1.setCursor(0, 0);
-  lcd1.print("     ##  ##    ");  // Глаза
+  lcd1.print("     ##  ##    ");
   lcd1.setCursor(0, 1);
-  lcd1.print("     ##  ##    ");  // Глаза
-  delay(500);                     // Задержка 1 секунда
+  lcd1.print("     ##  ##    ");
+  delay(1000); 
+}
 
-    // Выводим улыбку на втором дисплее
+void displaySmile() {
   lcd2.clear();
   lcd2.setCursor(0, 0);
-  lcd2.print("     ____    ");  // Верхняя часть улыбки
+  lcd2.print("     ____    ");
   lcd2.setCursor(0, 1);
-  lcd2.print("   /      \\  ");  // Рот
-
-  lcd1.clear();
-  lcd1.setCursor(0, 0);
-  lcd1.print("               ");  // Глаза
-  lcd1.setCursor(0, 1);
-  lcd1.print("     ##  ##    ");  // Глаза
-  delay(500);                     // Задержка 1 секунда
-
-
-  lcd1.clear();
-  lcd1.setCursor(0, 0);
-  lcd1.print("               ");  // Глаза
-  lcd1.setCursor(0, 1);
-  lcd1.print("     __  __    ");  // Глаза
-  delay(500);      
-
-  // Меняем улыбку на втором дисплее
+  lcd2.print("   /      \\  ");
+  delay(1000); 
+  
   lcd2.clear();
   lcd2.setCursor(0, 0);
-  lcd2.print("     ----    ");  // Верхняя часть улыбки
+  lcd2.print("     ----    ");
   lcd2.setCursor(0, 1);
-  lcd2.print("   \\______/  ");  // Рот
-  delay(500);                    // Задержка 1 секунда
+  lcd2.print("   \\______/  ");
+  delay(1000); 
+}
+
+void playSound(int track) {
+  if (myDFPlayer.available()) {
+    myDFPlayer.play(track);
+  }
+  
+  delay(5000); 
 }
